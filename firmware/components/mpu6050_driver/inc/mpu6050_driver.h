c9e063977e5e4bb4991cb4500188e30e50a7f3e7
Diego Bonilla <diegob8846@gmail.com>
2025-07-28 18:45:56 -0400

    Defined MPU6050 register macros and added MPU6050_data_t and MPU_Handle_t structs for organizing sensor data

---
+++ firmware/components/mpu6050_driver/inc/mpu6050_driver.h
@@ -0,0 +1,69 @@
+#ifndef MPU6050_DRIVER_H
+#define MPU6050_DRIVER_H
+
+#include <stdint.h>
+
+#define MPU6050_ADDR                          0x68
+
+#define MPU6050_WHO_AM_I_REG                  0x75
+
+// self test registers
+#define MPU6050_SELF_TEST_X_REG               0x0D
+#define MPU6050_SELF_TEST_Y_REG               0x0E
+#define MPU6050_SELF_TEST_Z_REG               0x0F
+
+//config registers
+#define MPU6050_SMPLRT_DIV_REG                0x19
+#define MPU6050_CONFIG_REG                    0x1A
+#define MPU6050_GYRO_CONFIG_REG               0x1B
+#define MPU6050_ACCEL_CONFIG_REG              0x1C
+#define MPU6050_FIFO_EN_REG                   0x23
+
+// Interrupt registers
+#define MPU6050_INT_PIN_CFG_REG               0x37
+#define MPU6050_INT_ENABLE_REG                0x38
+#define MPU6050_INT_STATUS_REG                0x3A
+
+// Accel registers
+#define MPU6050_ACCEL_XOUT_H_REG              0x3B
+#define MPU6050_ACCEL_XOUT_L_REG              0x3C
+#define MPU6050_ACCEL_YOUT_H_REG              0x3D
+#define MPU6050_ACCEL_YOUT_L_REG              0x3E
+#define MPU6050_ACCEL_ZOUT_H_REG              0x3F
+#define MPU6050_ACCEL_ZOUT_L_REG              0x40
+
+// Temp registers
+#define MPU6050_TEMP_OUT_H_REG                0x41
+#define MPU6050_TEMP_OUT_L_REG                0x42
+
+//Gyro registers
+#define MPU6050_GYRO_XOUT_H_REG               0x43
+#define MPU6050_GYRO_XOUT_L_REG               0x44
+#define MPU6050_GYRO_YOUT_H_REG               0x45
+#define MPU6050_GYRO_YOUT_L_REG               0x46
+#define MPU6050_GYRO_ZOUT_H_REG               0x47
+#define MPU6050_GYRO_ZOUT_L_REG               0x48
+
+// power managment register
+#define MPU6050_PWR_MGMT_1_REG                0x6B
+
+
+typedef struct {
+	float accel_x;   // Acceleration in g
+    float accel_y;
+    float accel_z;
+
+    float gyro_x;    // Angular velocity in deg/s
+    float gyro_y;
+    float gyro_z;
+
+}MPU6050_data_t;
+
+
+typedef struct {
+ 	MPU6050_data_t mpu_data;
+	uint8_t mpu_register;
+ } MPU_Handle_t;
+
+
+#endif
