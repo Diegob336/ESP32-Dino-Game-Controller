a1ca0ecd2ec040bb6605c22ef63a987f623a3ad0
Diego Bonilla <diegob8846@gmail.com>
2025-08-11 23:16:47 -0400

    Added UART communication component with basic command packet structure
    - Implemented Uart_init() to configure and initialize UART0
    - Added Uart_send_command() to send framed packets with start/end bytes and checksum
    - Defined dino_cmd_t enum for supported game commands
    - Implemented simple checksum calculation for payload verification
    - Uart_read_command() prototype added, implementation pending

---
+++ firmware/components/uart_comm/uart_comm.c
@@ -0,0 +1,38 @@
+#include "uart_comm.h"
+
+uint8_t calculate_checksum(uint8_t *data, uint16_t length);
+
+void Uart_init(){
+	uart_port_t uart_port_num = UART_NUM_0;
+
+	uart_config_t uart_config = {
+		.baud_rate = 115200,
+		.data_bits = UART_DATA_8_BITS,
+		.parity = UART_PARITY_DISABLE,
+		.stop_bits = UART_STOP_BITS_1,
+		.flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+	};
+
+	ESP_ERROR_CHECK(uart_driver_install(uart_port_num, 2048, 2048, 0, NULL, 0));
+	ESP_ERROR_CHECK(uart_param_config(uart_port_num, &uart_config));
+	ESP_ERROR_CHECK(uart_set_pin(uart_port_num, 1, 3, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+}
+void Uart_send_command(dino_cmd_t cmd){
+	uint8_t packet[4];
+	packet[0] = UART_COMM_START_BYTE;
+	packet[1] = cmd;
+	packet[2] = calulate_checksum(&cmd, 1); 
+	packet[3] = UART_COMM_END_BYTE;
+}
+void Uart_read_command(){
+
+}
+
+uint8_t calculate_checksum(uint8_t *data, uint16_t length) {
+	uint8_t sum = 0;
+	for (int i = 0; i < length; i++) {
+		sum += *(data + i);
+	}
+	return sum;
+}
+
---
+++ firmware/components/uart_comm/uart_comm.h
@@ -0,0 +1,19 @@
+#ifndef UART_COMM_H
+#define UART_COMM_H
+
+#include <stdint.h>
+#include "driver/uart.h"
+
+#define UART_COMM_START_BYTE    0xAA
+#define UART_COMM_END_BYTE      0x67
+
+typedef enum {
+	JUMP,
+	DUCK,
+} dino_cmd_t;
+
+void Uart_init();
+void Uart_send_command(dino_cmd_t cmd);
+void Uart_read_command();
+
+#endif 
