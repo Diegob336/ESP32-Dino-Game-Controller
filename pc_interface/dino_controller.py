897e60a6bf80f7b1e00ff1d87f3c9e3f3874306b
Diego <diegob8846@gmail.com>
2025-09-01 22:35:02 -0400

    Finished Python UART interface with checksum + end byte validation
    - Added checksum verification (sum of CMD & 0xFF) to ensure data integrity
    - Added END byte check against UART_COMM_END_BYTE

--- pc_interface/dino_controller.py
+++ pc_interface/dino_controller.py
@@ -1,25 +1,46 @@
-import serial;
+import serial
 
-import keyboard;
+import keyboard
+
+START_BYTE = 0xAA       # UART_COMM_START_BYTE
+END_BYTE   = 0x67      # UART_COMM_END_BYTE
+
+def compute_checksum(data_bytes):
+
+    return sum(data_bytes) & 0xFF
 
 
-ser = serial.Serial("/dev/ttyUSB0", 115200, serial.EIGHTBITS, serial.PARITY_NONE, timeout = None);
+ser = serial.Serial("/dev/ttyUSB0", 115200, serial.EIGHTBITS, serial.PARITY_NONE, timeout = None)
 
 
 while 1:
-    bytes_read = ser.read(1);
+    bytes_read = ser.read(1)
     
-    if  bytes_read[0] == 0xAA:
-        print("start bit recevied");
-        bytes_read = ser.read(1);
-        if bytes_read[0] == 1:
-            keyboard.press_and_release("space");
-            print("jump");
-        elif bytes_read[0] == 2:
-            keyboard.press_and_release("down");
-            print("duck");
+    if  bytes_read[0] == START_BYTE:
+        print("start bit recevied")
+
+        bytes_read = ser.read(3)
+        cmd = bytes_read[0]
+        rec_check = bytes_read[1]
+        end_byte = bytes_read[2]
+
+        if end_byte != END_BYTE:
+            print("Received end byte did not match correct end byte")
+            continue
+
+        calc_check = compute_checksum([cmd])
+        if rec_check != calc_check:
+            print("Received checksum and calculated checksum did not match")
+            continue
+
+        if cmd == 1:
+            keyboard.press_and_release("space")
+            print("jump")
+        elif cmd == 2:
+            keyboard.press_and_release("down")
+            print("duck")
+
+
         
-        ser.read(2);
-        
-     
+         
 
